d0095d5d5586286535b8ae35285032406b65de3c
Vadim Zub <vvzub@vtb.ru>
2023-05-17 07:33:49 +0300

    my first commit

---
+++ src/test/java/com/example/demo_testcontainers/DemoTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestcontainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
